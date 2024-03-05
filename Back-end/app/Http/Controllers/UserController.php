49c577289aea13c80b45d0f77fa406656daee979
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-03-05 01:55:48 +0100

    [Back-end]: Basic form storing and validation

--- Back-end/app/Http/Controllers/UserController.php
+++ Back-end/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Users;
 use Illuminate\Http\Request;
 use League\Flysystem\UrlGeneration\PublicUrlGenerator;
+use Mockery\Undefined;
 use Storage;
 
 // use Illuminate\Support\Facades\Storage;
@@ -26,16 +27,42 @@ class UserController extends Controller
     public function edit_user(Request $request)
     {
 
+        // getting users data
+        $first_name = $request->input('firstName');
+        $last_name = $request->input('lastName');
+        $age = (int) $request->input('age');
+        $status = $request->input('status');
+        $sexe = $request->input('sexe');
         $image = $request->file('image');
 
 
-        $image_path = $request->file('image')->store('public');
+        //  data validation 
+        $validate = $request->validate([
+            'firstName' => 'required|max:255',
+            'lastName' => 'required|max:255',
+            'age' => 'required|digits_between:1,100',
+            'status' => 'required',
+            'sexe' => 'required',
+        ]);
 
-        // TODO: store the relative path of the profile image in the DB.
-        $url = Storage::url($image_path);
+        $user = Users::find($request->id);
+        // store new updated profile image
+        if ($image !== null) {
+            $image_path = $request->file('public')->store($image);
+            $user->update(['image_path' => asset(Storage::url($image_path))]);
+        }
+
+        $user->update([
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'age' => $age,
+            'status' => $status,
+            'sex' => $sexe,
+        ]);
 
 
-        return json_encode(asset($url));
+
+        return json_encode(["200" => gettype($age)]);
 
     }
 
